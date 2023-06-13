c5da6c0b35e8c00633c383ba470bd8a55f6c2bed
welidwg <walidgueddari1899@gmail.com>
2023-06-13 18:46:53 +0100

    master

--- resources/views/GestionDeParametre/nav.blade.php
+++ resources/views/GestionDeParametre/nav.blade.php
@@ -1702,4 +1702,121 @@ return $resultset;
     }
 
     function closeForm() {
-        document.getElemen
+        document.getElementById("myForm").style.display = "none";
+    }
+</script>
+
+</html>
+<script>
+    //Get the button
+    var mybutton = document.getElementById("UP");
+    var navbar = document.getElementById("navbar");
+    var nom1 = document.getElementById("nom1");
+    var nom2 = document.getElementById("nom2");
+
+
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {
+        scrollFunction()
+    };
+
+    function Search(input, data) {
+        var input = document.getElementById(input);
+        var filter = input.value.toLowerCase();
+        var element = document.getElementsByClassName(data);
+
+
+        for (i = 0; i < element.length; i++) {
+
+            if (element[i].innerText.toLowerCase().includes(filter)) {
+                element[i].style.display = "table-row";
+
+
+            } else {
+                element[i].style.display = "none";
+
+            }
+        }
+    }
+
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            mybutton.style.display = "block";
+            navbar.style.background = "rgba(0,0,0,0.2)";
+            nom2.style.display = "block";
+            nom1.style.display = "none";
+
+        } else {
+            mybutton.style.display = "none";
+            navbar.style.background = "white";
+            nom2.style.display = "none";
+            nom1.style.display = "block";
+
+
+
+
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+
+
+    }
+</script>
+
+
+
+
+
+<script>
+    function maxLengthCheck(object) {
+        if (object.value.length > object.maxLength) {
+            object.value = object.value.slice(0, 10)
+
+
+        }
+    }
+</script>
+
+
+
+
+<script>
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>
+
+<?php
+
+} else {
+redirect()->to('index?LogIN')->send();
+}
+?>
